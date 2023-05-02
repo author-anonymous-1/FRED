738ff9787f2d82e11049f94263871b73da1d964d
Anonymous <author@anonymous.com>
2023-05-02 16:28:59 -0500

    Updates

--- sim.py
+++ sim.py
@@ -254,6 +254,141 @@ def gen_workload(cdf_app_service_times, cdf_num_jobs_per_app, cdf_max_gpus_per_j
 
         print("%d Apps generated" % (app_id+1))
 
+def run_sim(scheduling_policy, total_gpus, estimate, output_file, from_trace, cdf_app_service_times, cdf_num_jobs_per_app, cdf_max_gpus_per_job, cdf_min_gpus_per_job, load, num_gpus, num_apps, seed):
+    app_list = {}
+    event_queue = list()
+    if scheduling_policy in ["MCS", "PMCS", "MCS_PRIO"]:
+        if args.MCS_config_file == None:
+            class_detail = {"num_classes": 2, "class_thresholds": [6,float('inf')], "class_rates": [frac(50,100),frac(50,100)],
+                            "clip_demand_factor": 0.01, "delta": 0.01}
+
+        else:
+            with open(args.MCS_config_file, "rb") as fp:
+                class_detail = pickle.load(fp)
+        print(class_detail["num_classes"])
+        print(class_detail["class_thresholds"])
+        print(class_detail["class_rates"])
+
+        if scheduling_policy == "MCS":
+            scheduler = AppMCScheduler(total_gpus=total_gpus,
+                                        event_queue=event_queue,
+                                        app_list=app_list,
+                                        class_detail=class_detail,
+                                        app_info_fn=output_file,
+                                        estimate=estimate)
+        elif scheduling_policy == "PMCS":
+            scheduler = AppPracticalMCScheduler(total_gpus=total_gpus,
+                                        event_queue=event_queue,
+                                        app_list=app_list,
+                                        class_detail=class_detail,
+                                        quantum=100,
+                                        app_info_fn=output_file,
+                                        estimate=estimate)
+        elif scheduling_policy == "MCS_PRIO":
+            def mcs_prio_func(app_id_to_end_times, a):
+                return app_id_to_end_times[a.app_id]
+
+            with open("MCS.csv") as fp:
+                key, *fdata = fp.readlines()
+            app_id_to_end_times = {}
+            for entry in fdata:
+                app_id,_,_,end_time,*_ = entry.split(',')
+                app_id_to_end_times[int(app_id)] = float(end_time)
+
+
+            scheduler = AppPrioScheduler(total_gpus=total_gpus,
+                                        event_queue=event_queue,
+                                        app_list=app_list,
+                                        prio_func=partial(mcs_prio_func, app_id_to_end_times),
+                                        app_info_fn=output_file,
+                                        estimate=estimate)
+
+        else:
+            raise NotImplementedError
+
+    elif scheduling_policy == "FIFO":
+        scheduler = AppPrioScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    prio_func=lambda a: a.submit_time,
+                                    app_info_fn=output_file,
+                                    estimate=estimate)
+    elif scheduling_policy == "SRTF":
+        scheduler = AppPrioScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    prio_func=lambda a: a.remaining_service/a.demand,
+                                    app_info_fn=output_file,
+                                    estimate=estimate)
+    elif scheduling_policy == "SRSF":
+        scheduler = AppPrioScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    prio_func=lambda a: a.remaining_service,
+                                    app_info_fn=output_file,
+                                    estimate=estimate)
+    elif scheduling_policy == "LAS":
+        scheduler = AppPrioScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    prio_func=lambda a: a.service - a.remaining_service,
+                                    app_info_fn=output_file,
+                                    estimate=estimate)
+    elif scheduling_policy == "FS":
+        scheduler = AppFairScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    app_info_fn=output_file,
+                                    estimate=estimate)
+    elif scheduling_policy == "PFS":
+        scheduler = AppPracticalFairScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    quantum=1,
+                                    app_info_fn=output_file,
+                                    estimate=0)
+    elif scheduling_policy == "THEMIS":
+        scheduler = AppThemisScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    app_info_fn=output_file,
+                                    estimate=estimate)
+    elif scheduling_policy == "AFS":
+        scheduler = AppAFSScheduler(total_gpus=total_gpus,
+                                    event_queue=event_queue,
+                                    app_list=app_list,
+                                    app_info_fn=output_file,
+                                    estimate=estimate)
+    else:
+        raise NotImplementedError
+    
+
+    if from_trace:
+        gen_workload_from_trace("workload.csv", app_list, event_queue)
+    else:
+        gen_workload(cdf_app_service_times,
+                    cdf_num_jobs_per_app,
+                    cdf_max_gpus_per_job,
+                    cdf_min_gpus_per_job,
+                    load,
+                    num_gpus,
+                    num_apps,
+                    seed,
+                    app_list,
+                    event_queue)
+
+    print("Starting sim with %d Apps" % len(event_queue))
+    tick = datetime.now()
+    scheduler.run()
+    tock = datetime.now()
+    print(f"\nsim took {(tock - tick).total_seconds()} secs")
+    print(f"average_num_jobs: {scheduler._avg_contention.avg}")
+    if scheduler._num_finished_apps != len(app_list):
+        print("Cluster is not big enough for largest job")
+        sys.exit(1)
+    print("\nSim ended.")
+
+
 if __name__ == '__main__':
 
 
@@ -285,183 +420,13 @@ if __name__ == '__main__':
     total_gpus=args.num_gpus
     estimate = args.estimate
     output_file = args.output_file
-
-
-
-    app_list = {}
-    event_queue = list()
-
-
-    # total_gpus, event_queue, app_list, class_detail, app_info_fn="results.csv", suppress_print=False
-    
-    if scheduling_policy in ["MCS", "PMCS", "MCS_PRIO"]:
-
-
-
-        if args.MCS_config_file == None:
-            # class_detail = {"num_classes": 3, "class_thresholds": [1523, 5088, float('inf')], "class_rates": [frac(889,1000),frac(1,10),frac(11,1000)]}
-            # class_detail = {"num_classes": 2, "class_thresholds": [3, float('inf')], "class_rates": [frac(950,1000),frac(50,1000)]}
-            class_detail = {"num_classes": 2, "class_thresholds": [6,float('inf')], "class_rates": [frac(50,100),frac(50,100)],
-                            "clip_demand_factor": 0.01, "delta": 0.01}
-
-        else:
-            with open(args.MCS_config_file, "rb") as fp:
-                class_detail = pickle.load(fp)
-
-
-
-        # class_detail = {"num_classes": 1, "class_thresholds": [float('inf')], "class_rates": [1.0]}
-        # class_detail = {"num_classes": 3, "class_thresholds": [200,300,float('inf')], "class_rates": [0.85,0.1,0.05]}
-        # class_detail = {"num_classes": 2, "class_thresholds": [720000,float('inf')], "class_rates": [0.75,0.25]}
-        # class_detail = {"num_classes": 2, "class_thresholds": [36000.0, float('inf')], "class_rates": [0.75,0.25]}
-        # class_detail = {"num_classes": 2, "class_thresholds": [500.0, float('inf')], "class_rates": [0.67,0.33]}
-        # class_detail = {"num_classes": 2, "class_thresholds": [720000, float('inf')], "class_rates": [frac(88,100),frac(12,100)]}
-        # class_detail = {"num_classes": 2, "class_thresholds": [3173, float('inf')], "class_rates": [frac(88,100),frac(12,100)]}
-        # class_detail = {"num_classes": 2, "class_thresholds": [4000, float('inf')], "class_rates": [frac(47,50),frac(3,50)]}
-        # class_detail = {"num_classes": 3, "class_thresholds": [1523, 5088, float('inf')], "class_rates": [frac(889,1000),frac(1,10),frac(11,1000)]}
-
-        # class_detail = {"num_classes": 2, "class_thresholds": [4800, float('inf')], "class_rates": [0.92,0.08]}
-
-        print(class_detail["num_classes"])
-        print(class_detail["class_thresholds"])
-        print(class_detail["class_rates"])
-
-
-        if scheduling_policy == "MCS":
-            scheduler = AppMCScheduler(total_gpus=total_gpus,
-                                        event_queue=event_queue,
-                                        app_list=app_list,
-                                        class_detail=class_detail,
-                                        app_info_fn=output_file,
-                                        estimate=args.estimate)
-        elif scheduling_policy == "PMCS":
-            scheduler = AppPracticalMCScheduler(total_gpus=total_gpus,
-                                        event_queue=event_queue,
-                                        app_list=app_list,
-                                        class_detail=class_detail,
-                                        quantum=100,
-                                        app_info_fn=output_file,
-                                        estimate=args.estimate)
-
-
-        elif scheduling_policy == "MCS_PRIO":
-
-            def mcs_prio_func(app_id_to_end_times, a):
-                return app_id_to_end_times[a.app_id]
-
-            with open("MCS.csv") as fp:
-                key, *fdata = fp.readlines()
-
-            
-            app_id_to_end_times = {}
-            for entry in fdata:
-                app_id,_,_,end_time,*_ = entry.split(',')
-                app_id_to_end_times[int(app_id)] = float(end_time)
-
-
-            scheduler = AppPrioScheduler(total_gpus=total_gpus,
-                                        event_queue=event_queue,
-                                        app_list=app_list,
-                                        prio_func=partial(mcs_prio_func, app_id_to_end_times),
-                                        app_info_fn=output_file,
-                                        estimate=args.estimate)
-
-        else:
-            raise NotImplementedError
-
-    elif scheduling_policy == "FIFO":
-        scheduler = AppPrioScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    prio_func=lambda a: a.submit_time,
-                                    app_info_fn=output_file,
-                                    estimate=args.estimate)
-    elif scheduling_policy == "SRTF":
-        scheduler = AppPrioScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    prio_func=lambda a: a.remaining_service/a.demand,
-                                    app_info_fn=output_file,
-                                    estimate=args.estimate)
-    elif scheduling_policy == "SRSF":
-        scheduler = AppPrioScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    prio_func=lambda a: a.remaining_service,
-                                    app_info_fn=output_file,
-                                    estimate=args.estimate)
-    elif scheduling_policy == "LAS":
-        scheduler = AppPrioScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    prio_func=lambda a: a.service - a.remaining_service,
-                                    app_info_fn=output_file,
-                                    estimate=args.estimate)
-
-
-
-    elif scheduling_policy == "FS":
-        scheduler = AppFairScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    app_info_fn=output_file,
-                                    estimate=args.estimate)
-    elif scheduling_policy == "PFS":
-        scheduler = AppPracticalFairScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    quantum=1,
-                                    app_info_fn=output_file,
-                                    estimate=0)
-
-    elif scheduling_policy == "THEMIS":
-        scheduler = AppThemisScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    app_info_fn=output_file,
-                                    estimate=args.estimate)
-    elif scheduling_policy == "AFS":
-        scheduler = AppAFSScheduler(total_gpus=total_gpus,
-                                    event_queue=event_queue,
-                                    app_list=app_list,
-                                    app_info_fn=output_file,
-                                    estimate=args.estimate)
-    else:
-        raise NotImplementedError
-
-
-    if args.from_trace:
-        gen_workload_from_trace("workload.csv", app_list, event_queue)
-    else:
-        gen_workload(args.cdf_app_service_times,
-                    args.cdf_num_jobs_per_app,
-                    args.cdf_max_gpus_per_job,
-                    args.cdf_min_gpus_per_job,
-                    args.load,
-                    args.num_gpus,
-                    args.num_apps,
-                    args.seed,
-                    app_list,
-                    event_queue)
-    
-    
-
-    print("Starting sim with %d Apps" % len(event_queue))
-
-    tick = datetime.now()
-    scheduler.run()
-    tock = datetime.now()
-
-    print(f"\nsim took {(tock - tick).total_seconds()} secs")
-
-    print(f"average_num_jobs: {scheduler._avg_contention.avg}")
-
-    
-
-
-    if scheduler._num_finished_apps != len(app_list):
-        print("Cluster is not big enough for largest job")
-        sys.exit(1)
-
-
-    print("\nSim ended.")
+    from_trace = args.from_trace
+    cdf_app_service_times = args.cdf_app_service_times
+    cdf_num_jobs_per_app = args.cdf_num_jobs_per_app
+    cdf_max_gpus_per_job = args.cdf_max_gpus_per_job
+    cdf_min_gpus_per_job = args.cdf_min_gpus_per_job
+    load = args.load
+    num_gpus = args.num_gpus
+    num_apps = args.num_apps
+    seed = args.seed
+    run_sim(scheduling_policy, total_gpus, estimate, output_file, from_trace, cdf_app_service_times, cdf_num_jobs_per_app, cdf_max_gpus_per_job, cdf_min_gpus_per_job, load, num_gpus, num_apps, seed)
